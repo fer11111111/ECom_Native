aa024c112da182cf86e8e93a656b06382047d125
fer11111111 <info.test2303@gmail.com>
2025-01-26 01:33:54 +0530

    navbar

--- src/types.ts
+++ src/types.ts
@@ -1,14 +1,22 @@
 // src/types.ts
 export interface Product {
-    sys: {
-      id: string;
-    };
-    fields: {
-      name: string;
-      price: number;
-      media: string;
-    };
-  }
-  export interface ContentfulResponse {
-    items: Product[];
-  }
+  sys: {
+    id: string;
+  };
+  fields: {
+    name: string;
+    price: number;
+    media: string;
+  };
+}
+
+export interface Category {
+  fields: {
+    categoryName: string;
+  };
+}
+
+export interface ContentfulResponse {
+  items: Product[];
+  cates : Category[];
+}
